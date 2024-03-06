e2f4ce8415cb5f50c9aab85efad509587036ebb9
ERNESTOAD <ernestoantoniodavila@gmail.com>
2024-03-05 18:48:57 -0600

    se suben los archivos a git

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ function triangulo(){
     document.getElementById('res').value=res;
 }
 function cuadrado(){
-    let num1= Number(document.getElementById('n1').value);
-    let num2=Number(document.getElementById('n2').value);
-    let res= num1*num2;
-    document.getElementById('res').value=res;
+    let L1= Number(document.getElementById('n3').value);
+    let res= L1*L1;
+    document.getElementById('res1').value=res; 
 }
 function rectangulo(){
-    let num1= Number(document.getElementById('n1').value);
-    let num2=Number(document.getElementById('n2').value);
-    let res= num1*num1;
-    document.getElementById('res').value=res;
+    let base1= Number(document.getElementById('n4').value);
+    let altura2=Number(document.getElementById('n5').value);
+    let res= base1*altura2;
+    document.getElementById('res2').value=res;
 }
 function circulo(){
-    let num1= Number(document.getElementById('n1').value);
-    let num2=Number(document.getElementById('n2').value);
+    let num1= Number(document.getElementById('n6').value);
     let b=num1*num1;
     let res= b*3.1416;
-    document.getElementById('res').value=res;
+    document.getElementById('res3').value=res;
 }
